3d7013f3118c4bb5b36cd90ef8a8d05d25ce79b2
Geração Arrábida <geracaoarrabida@gmail.com>
2025-08-01 01:36:34 +0000

    ds

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, render_template_string, redirect, make_response
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.utils import *
 
 app = Flask(__name__)
